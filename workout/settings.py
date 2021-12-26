30518d5428779a4c1131a6094ff4c0ca579a0a6f
Oleh Vostrilkov <grine1108@gmail.com>
2021-12-26 19:06:46 +0200

    first commit

--- workout/settings.py
+++ workout/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
